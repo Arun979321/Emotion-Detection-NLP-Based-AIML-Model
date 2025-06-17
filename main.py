b314c93ae387da07bfb89f1aa6b9dd08a8f94141
Arun979321 <148804375+Arun979321@users.noreply.github.com>
2025-06-17 09:22:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import joblib
+import subprocess
+import streamlit as st
+
+installed = subprocess.getoutput("pip list")
+st.text(installed)
+
+
+# Load trained model and tools
+model = joblib.load('emotion_model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
+label_encoder = joblib.load('label_encoder.pkl')
+
+# Prediction function
+def predict_emotion(text):
+    vec = vectorizer.transform([text])
+    label = model.predict(vec)[0]
+    return label_encoder.inverse_transform([label])[0]
+
+# UI
+st.title("Emotion Classifier")
+st.write("Enter your message and let the model guess the emotion!")
+
+user_input = st.text_area("Enter a message:")
+
+if st.button("Predict Emotion"):
+    if user_input.strip():
+        emotion = predict_emotion(user_input)
+        st.success(f"Predicted Emotion: **{emotion}**")
+    else:
+        st.warning("Please enter some text.")
